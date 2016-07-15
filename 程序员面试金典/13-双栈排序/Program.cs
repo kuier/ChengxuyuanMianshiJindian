0884745cc00209c167e6022ea417168a0bfe4058
kuier <838113965@qq.com>
2016-07-15 14:46:50 +0800

    栈排序

---
+++ 程序员面试金典/13-双栈排序/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * 请编写一个程序，按升序对栈进行排序（即最大元素位于栈顶），要求最多只能使用一个额外的栈存放临时数据，但不得将元素复制到别的数据结构中。
+给定一个int[] numbers(C++中为vector&ltint>)，其中第一个元素为栈顶，请返回排序后的栈。请注意这是一个栈，意味着排序过程中你只能访问到第一个元素。
+ */
+namespace _13_双栈排序
+{
+    class Program
+    {
+        class TwoStacks
+        {
+            public List<int> twoStacksSort(int[] numbers)
+            {
+                // write code here
+                List<int> result = new List<int>();
+                
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
